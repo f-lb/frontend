95a3466b88a5b4cf274cb2e3e04e3d227e8d0438
kimtaejin3 <rlaxowls1316@naver.com>
2024-07-16 13:17:44 +0900

    RootLayout 생성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AnalyzePage, CalendarPage } from "./pages";
-import { BnbLayout } from "./layouts";
+import { BnbLayout, RootLayout } from "./layouts";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route element={<BnbLayout />}>
-            <Route path="/" element={<CalendarPage />} />
-            <Route path="/analyze" element={<AnalyzePage />} />
-          </Route>
-      </Routes>
-    </BrowserRouter>
+    <RootLayout>
+      <BrowserRouter>
+        <Routes>
+            <Route element={<BnbLayout />}>
+              <Route path="/" element={<CalendarPage />} />
+              <Route path="/analyze" element={<AnalyzePage />} />
+            </Route>
+        </Routes>
+      </BrowserRouter>
+    </RootLayout>
   );
 }
 
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,1 +1,2 @@
 export { default as BnbLayout } from "./BnbLayout";
+export { default as RootLayout} from "./RootLayout";
