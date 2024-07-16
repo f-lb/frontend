b6e429a81e186b1e5d43442944fc7185d325b9e3
kimtaejin3 <rlaxowls1316@naver.com>
2024-07-16 13:00:02 +0900

    BnbLayout 라우터에 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AnalyzePage, CalendarPage } from "./pages";
+import { BnbLayout } from "./layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
-          <Route path="/" element={<CalendarPage />} />
-          <Route path="/analyze" element={<AnalyzePage />} />
-        </Route>
+          <Route element={<BnbLayout />}>
+            <Route path="/" element={<CalendarPage />} />
+            <Route path="/analyze" element={<AnalyzePage />} />
+          </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/layout/index.js
@@ -0,0 +1,1 @@
+export { default as BnbLayout } from "./BnbLayout";
