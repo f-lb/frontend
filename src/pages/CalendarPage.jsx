cc3f1d1991a09ef8a25b4c9c989e2af3d7d42e50
kimtaejin3 <rlaxowls1316@naver.com>
2024-07-20 23:06:31 +0900

    calendar toggle btn 완성

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -2,7 +2,10 @@ import styled from "styled-components";
 import BellIcon from "../assets/bell.svg";
 import ArrowIcon from "../assets/arrow.svg";
 import CalendarSIcon01 from "../assets/calendar_s01.svg";
+import CalendarSIcon02 from "../assets/calendar_s02.svg";
+import Filled_CalendarSIcon01 from "../assets/calendar_s01_filled.svg";
 import Filled_CalendarSIcon02 from "../assets/calendar_s02_filled.svg";
+import { useState } from "react";
 
 const mockDatas = [
   {
@@ -295,6 +298,8 @@ const mockDatas = [
 ];
 
 export default function CalendarPage() {
+  const [calendarType, setCalendarType] = useState("monthlyMode");
+
   return (
     <Container>
       <Header>
@@ -317,11 +322,27 @@ export default function CalendarPage() {
           </YearCtrl>
         </DateCtrl>
         <CalendarToggle>
-          <ToggleBtn>
-            <img src={CalendarSIcon01} alt="calendar icon" />
+          <ToggleBtn
+            onClick={() => setCalendarType("monthlyMode")}
+            $isActive={calendarType === "monthlyMode"}
+            $isMonthlyMode
+          >
+            {calendarType === "monthlyMode" ? (
+              <img src={Filled_CalendarSIcon01} alt="calendar icon filled" />
+            ) : (
+              <img src={CalendarSIcon01} alt="calendar icon" />
+            )}
           </ToggleBtn>
-          <ToggleBtn>
-            <img src={Filled_CalendarSIcon02} alt="calendar icon" />
+          <ToggleBtn
+            onClick={() => setCalendarType("weeklyMode")}
+            $isActive={calendarType === "weeklyMode"}
+            $isMonthlyMode={false}
+          >
+            {calendarType === "weeklyMode" ? (
+              <img src={Filled_CalendarSIcon02} alt="calendar icon filled" />
+            ) : (
+              <img src={CalendarSIcon02} alt="calendar icon" />
+            )}
           </ToggleBtn>
         </CalendarToggle>
       </CalendarCtrl>
@@ -410,14 +431,12 @@ const CalendarToggle = styled.div`
 const ToggleBtn = styled.button`
   padding: 3px 7px;
   height: 31px;
-  &:nth-child(2){
-    border-left: 2px solid #bfbfbf;
-    overflow: clip
-    background-color:red;
-    border-top-left-radius: 7px;
-    border-bottom-left-radius: 7px;
-    background-color: #A4BFED;
-  }
+
+  ${({ $isActive, $isMonthlyMode }) =>
+    $isActive &&
+    ($isMonthlyMode
+      ? "background-color: #a4bfed; border-right: 3px solid #bfbfbf; border-top-right-radius: 7px;border-bottom-right-radius: 7px;"
+      : "background-color: #a4bfed; border-left: 3px solid #bfbfbf; border-top-left-radius: 7px; border-bottom-left-radius: 7px")}
 `;
 
 const WeeksBar = styled.ul`
@@ -444,7 +463,7 @@ const Calendar = styled.ul`
   background-color: #fff;
 `;
 
-const Date = styled.li`
+const Date = styled.div`
   cursor: pointer;
   text-align: center;
   color: #363636;
@@ -531,8 +550,4 @@ const StatusBar = styled.div`
   left: 50%;
   transform: translateX(-50%);
   top: -8px;
-`;
-
-const DateBox = styled.div`
-  margin: 0 auto;
 `;
