a62eae03bd80ee611a01b296be62bb261fe78938
kimtaejin3 <rlaxowls1316@naver.com>
2024-07-31 14:53:57 +0900

    일기작성페이지 nav 완성

--- src/pages/RecordPage.jsx
+++ src/pages/RecordPage.jsx
@@ -1,3 +1,28 @@
+import styled from "styled-components";
+import BackIcon from "../assets/back.svg";
+import { Link } from "react-router-dom";
+
 export default function RecordPage() {
-  return <>Record page</>;
+  return (
+    <>
+      <Nav>
+        <Link style={{ position: "absolute", left: "15px" }}>
+          <img src={BackIcon} alt="back_icon" />
+        </Link>
+        일기
+      </Nav>
+    </>
+  );
 }
+
+const Nav = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 48px;
+  font-size: 18px;
+  font-weight: bold;
+  background: #fff;
+`;
