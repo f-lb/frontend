9c9da985f8de27f3179b4582c5122cbd48d14d7f
kimtaejin3 <rlaxowls1316@naver.com>
2024-08-03 04:55:27 +0900

    http -> https

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://43.201.49.70:8080/api",
+  baseURL: "https://43.201.49.70:8080/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
