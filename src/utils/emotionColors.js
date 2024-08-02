f8d2b4d601106647d03edbfd5c5505d9f8e5a348
SEOHYON OH <skibb27@gamil.com>
2024-08-02 10:37:03 +0900

    객체 완성

---
+++ src/utils/emotionColors.js
@@ -0,0 +1,37 @@
+const emotionColors = {
+    emotions: {
+      happiness: {
+        number: 0,
+        color: "#F8DF6C",
+        imageUrl: "src/assets/happyCard.svg"
+      },
+      surprised: {
+        number: 1,
+        color: "#FFB5E9",
+        imageUrl: "src/assets/surprisedCard.svg"
+      },
+      anxiety: {
+        number: 3,
+        color: "#A9E295",
+        imageUrl: "src/assets/anxietyCard.svg"
+      },
+      anger: {
+        number: 2,
+        color: "#A9E295",
+        imageUrl: "src/assets/angeryCard.svg"
+      },
+      sadness: {
+        number: 4,
+        color: "#C5D6F5",
+        imageUrl: "/src/assets/sadCard.svg"
+      },
+      neutrality: {
+        number: 5,
+        color: "#C5C5C5",
+        imageUrl: "src/assets/neutralCard.svg"
+      }
+    }
+  };
+  
+  export default emotionColors;
+  
